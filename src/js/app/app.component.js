e5d5901188998ddad85e2276a9b2bdb9a7b9cd28
Eric W. Greene <eric@ericwgreene.com>
2017-05-15 12:59:12 -0500

    added code demos

--- src/js/app/app.component.js
+++ src/js/app/app.component.js
@@ -3,19 +3,33 @@
 
 // black, white, red, green, saffron, blue
 
-
-function AppComponent() {
+function AppComponent($scope) {
 
     var vm = this;
 
     vm.message = 'Hello World!';
     vm.newColor = '';
 
+    vm.lastColors = null;
     vm.colors = [ 'black', 'white', 'red', 'green', 'saffron', 'blue' ];
 
     vm.addColor = function() {
-        vm.colors.push(vm.newColor);    
+        vm.colors = vm.colors.concat(vm.newColor);    
     };
+
+    vm.sortedColors = function() {
+        console.log('get sorted colors');
+        if (vm.colors !== vm.lastColors) {
+            console.log('sorting colors');
+            vm.colors.sort();
+            vm.lastColors = vm.colors;
+        }
+        return vm.colors;
+    };
+
+    $scope.$watch(function() {
+        console.log('running change detection (digest loop)');
+    });
 }
 
 export var appComponent = [ 'main', function() {
--- src/js/app/app.module.js
+++ src/js/app/app.module.js
@@ -1,4 +1,13 @@
 import { appComponent } from './app.component';
 
 export var appModule = angular.module('App', [])
+    .filter('mySort', function() {
+    
+        return function(list) {
+            console.log('doing list sort');
+            list.sort();
+            return list;
+        };
+
+    })
     .directive(appComponent[0], appComponent[1]);

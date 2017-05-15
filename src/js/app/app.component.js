3b5a807f2a8964ff5316f038b5db873b0460e49d
Eric W. Greene <eric@ericwgreene.com>
2017-05-15 11:43:47 -0500

    added code demos

--- src/js/app/app.component.js
+++ src/js/app/app.component.js
@@ -1,7 +1,21 @@
 // import { componentHelper } from './utils/component';
 
+
+// black, white, red, green, saffron, blue
+
+
 function AppComponent() {
-    this.message = 'Hello World!';
+
+    var vm = this;
+
+    vm.message = 'Hello World!';
+    vm.newColor = '';
+
+    vm.colors = [ 'black', 'white', 'red', 'green', 'saffron', 'blue' ];
+
+    vm.addColor = function() {
+        vm.colors.push(vm.newColor);    
+    };
 }
 
 export var appComponent = [ 'main', function() {
